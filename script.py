52a11b6f883088ae42eb6d09e15787cdfab734a4
jjlema <jjlema@gmail.com>
2026-01-21 22:12:28 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-
+EMAIL_TO = [email.strip() for email in os.environ["EMAIL_TO"].split(",")]
+EMAIL_FROM = os.environ["EMAIL_FROM"]
+EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
 URL_CUPO_MEDICO = "https://tsinternet.sergas.es/TSInternet/ConsultaHorariosMedicos.servlet?CUPO=#id_medico#&orixe=TSI"
-IDS_CUPO = ["36342462", "36342866", "36341957", "36341452"]
+IDS_MEDICO = [id.strip() for id in os.environ["IDS_MEDICO"].split(",")]
 
 
-for id_medico in IDS_CUPO:
+def is_medico_available(id_medico: str) -> tuple[str,bool]:
     url = URL_CUPO_MEDICO.replace("#id_medico#", id_medico)
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     medico = soup.find('input', {'name': 'nomemedico'})['value']
-    cupo = [inp['value'] for inp in soup.find_all('input', {'name': 'telfurx'}) if 'value' in inp.attrs]
-    print(f'Médico: {medico}, Cupo: {cupo}')
+    cupos = [inp['value'] for inp in soup.find_all('input', {'name': 'telfurx'}) if 'value' in inp.attrs]
+    return medico, any(cupo == 'ABERTO' for cupo in cupos)
+
+
+def send_email_notification(medicos:list[str]):
+    msg = MIMEMultipart()
+    msg["From"] = EMAIL_FROM
+    msg["To"] = ", ".join(EMAIL_TO)
+    msg["Subject"] = "Medico disponible"
+    texto = "Los siguientes médicos tienen citas disponibles:\n\n" + "\n".join(medicos)
+    msg.attach(MIMEText(texto,"plain"))
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:  # Cambia si usas otro servicio
+            server.starttls()  # Seguridad TLS
+            server.login(EMAIL_FROM, EMAIL_PASSWORD)
+            server.send_message(msg)
+        print(f"Correo enviado correctamente a {EMAIL_TO}")
+    except Exception as e:
+        print(f"Error al enviar correo: {e}")
+
+
+def main():
+    medicos_disponibles = []
+    for id_medico in IDS_MEDICO:
+        medico, disponible = is_medico_available(id_medico)
+        if disponible:
+            medicos_disponibles.append(medico)
+    if medicos_disponibles:
+        send_email_notification(medicos_disponibles)
+    else:
+        print("No hay médicos disponibles en este momento.")
+
+if __name__ == "__main__":
+    main()
